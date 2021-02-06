467fadf40888906f4cc7473ea8f268ca1f17ae6e
DELL <DELL@DESKTOP-M4DCJF6>
2021-02-06 18:11:09 +0530

    First commit

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+
+public class Test {
+String str;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str="amaravani";
+System.out.println(str.charAt(4));
+	}
+
+}
